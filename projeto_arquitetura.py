3b2828f6ea4062d4b1cf02ed8b3459f07359f742
AdrianoDevPlay <160899241+Adrianodev00play@users.noreply.github.com>
2026-06-16 18:27:09 -0300

    Refactor file creation and listing in GUI

--- projeto_arquitetura.py
+++ projeto_arquitetura.py
@@ -1,10 +1,13 @@
 import os
 import tkinter as tk
+import keyboard as ky
 os.system("cls")
 
+def ok():
+    campo.delete(0, tk.END)
+    campo.pack()
+    confirmar.pack()
 def criar():
-    campo=tk.Entry(janela)
-    campo.pack( )
     texto=campo.get()
     if texto!="":
         for i in range(len(os.listdir())):
@@ -12,20 +15,29 @@ def criar():
         with open(f"{texto}.py", "w"):
             pass
         listar()
+    campo.pack_forget()
+    confirmar.pack_forget()
+    janela.update()
     
 def listar():
     arquivo.append("")
     i=0
     for item in os.listdir():
-        arquivo[i]=tk.Label(janela, text=item)
-        arquivo[i].pack(pady=1)
+        arquivo[i]=tk.Label(frame_arquivos, text=item, image=imagem, compound="top", wraplength=80, bg="gray")
+        arquivo[i].pack(side="left", padx=5)
         i=i+1
         
 janela=tk.Tk()
 janela.geometry("500x300")
 janela.config(bg="gray")
+imagem=tk.PhotoImage(file="teste.png")
+mais=tk.PhotoImage(file="mais.png")
 arquivo=[""]*len(os.listdir())
-botao=tk.Button(janela, text="criar arquivos", command=criar, bg="yellow")
-botao.pack(pady=10)
+botao=tk.Button(janela, command=ok, image=mais, bg="gray", bd=0)
+botao.pack(pady=20)
+frame_arquivos = tk.Frame(janela, bg="gray")
+frame_arquivos.pack()
 listar()
+campo=tk.Entry(janela)
+confirmar=tk.Button(janela, text="OK", command=criar, bg="yellow")
 janela.mainloop()
