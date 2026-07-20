562aa44f091c02e44814acf3d40907d404a0c191
AdrianoDevPlay <160899241+Adrianodev00play@users.noreply.github.com>
2026-07-19 23:25:57 -0300

    Enhance file management features and UI
    Refactor file creation and editing functionality, add window management.

--- projeto_arquitetura.py
+++ projeto_arquitetura.py
@@ -1,106 +1,144 @@
 import os
 import tkinter as tk
 import sys
-import os
+
+def fechar_todas_janelas():
+    janela.destroy()
+    edit.destroy()
+    nova_janela.destroy()
+def renomear_update_arquivo(antigo):
+    texto=campo.get()
+    if antigo.endswith(".py"):
+        os.rename(antigo, f"{texto}.py")
+    else:
+        os.rename(antigo, texto)
+    campo.pack_forget()
+    confirmar.pack_forget()
+    edit.destroy()
+    limpar_arquivos_da_tela()
+    listar()
+def valor_pasta_0():
+    global pasta
+    pasta=0
+    nova_janela.destroy()
+def janela_edicao(evento, nome):
+    global edit
+    edit=tk.Tk()
+    x=(edit.winfo_screenwidth()//2)-(200//2)
+    y=(edit.winfo_screenheight()//2)-(100//2)
+    edit.geometry(f"{200}x{100}+{x}+{y}")
+    apagar=tk.Button(edit, text="apagar", command=lambda:apagar_arquivos(1, nome))
+    apagar.pack()
+    renomear=tk.Button(edit, text="renomear", command=lambda:(criar_campo_digitavel(edit, renomear_update_arquivo, nome), renomear.pack_forget(), apagar.pack_forget()))
+    renomear.pack()
+    edit.mainloop()
+
+def criar_campo_digitavel(tipo, funcao, nome):
+    global campo
+    global confirmar
+    
+    campo=tk.Entry(tipo)
+    campo.pack(pady=10)
+    confirmar=tk.Button(tipo, text="OK", command=lambda:funcao(nome), bg="blue", fg="white")
+    confirmar.pack()
+    campo.delete(0, tk.END)
+
+def apagar_arquivos(event, texto):
+    limpar_arquivos_da_tela()
+    if texto.endswith(".py"):
+        os.remove(texto)
+    else:
+        os.rmdir(texto)
+    listar()
+    edit.destroy()
+
+def verificar_o_arquivo_selecionado(texto):
+    if pasta==1:
+        os.mkdir(texto)
+    else:
+        with open(f"{texto}.py", "w"):
+            pass
+    listar()
+def limpar_arquivos_da_tela():
+    for i in range(len(os.listdir())):
+            arquivo[i].destroy()
+    
+def verificar_arquivo_e_adiciona_imagem(item, i):
+    if item.endswith(".py"):
+        arquivo[i].config(image=imagem)
+    else:
+        arquivo[i].config(image=diretorio)
+
+def criar_arquivos_e_adicionar_ao_vetor(linha, coluna, i, item):
+    caixa = tk.Frame(frame_arquivos, width=120, height=140, bg="#2e2d2d")
+    caixa.grid(row=linha, column=coluna, padx=10, pady=10)
+    caixa.grid_propagate(False)
+    arquivo[i] = tk.Button(caixa, text=item, compound="top", wraplength=100, bg="#2e2d2d", fg="white", font=("Segoe UI Variable Small Semibol", 10), bd=0)
+    arquivo[i].bind("<Double-Button-1>", lambda event: apagar_arquivos(event, item))
+    arquivo[i].bind("<Button-3>", lambda event: janela_edicao(event, item))
+
+
 
 def caminho(relativo):
     base = getattr(sys, "_MEIPASS", os.path.abspath("."))
     return os.path.join(base, relativo)
 os.system("cls")
 
+
 #função que confirma a criação de um novo arquivo
-def ok(a):
+def selecao_de_arquivo(identificador):
     global pasta
     global campo
     global confirmar
-    if a==1:
+    if identificador==1:
         pasta=1
-    criar_arquivo.pack_forget()
-    criar_pasta.pack_forget()
-    campo=tk.Entry(asd)
-    campo.pack(pady=10)
-    confirmar=tk.Button(asd, text="OK", command=criar, bg="blue", fg="white")
-    confirmar.pack()
-    campo.delete(0, tk.END)
-
+    criar_campo_digitavel(nova_janela, criar_um_novo_arquivo, 1)    
 #função que escolhe o tipo de arquivo
-def file():
+def abrir_janela_ciação_arquivo():
     global criar_pasta
     global criar_arquivo
-    global asd
-    asd=tk.Toplevel()
-    asd.config(bg="gray")
-    x=(asd.winfo_screenwidth()//2)-(200//2)
-    y=(asd.winfo_screenheight()//2)-(100//2)
-    asd.geometry(f"{200}x{100}+{x}+{y}")
-    criar_pasta=tk.Button(asd, text="Pasta", command=lambda:ok(1))
+    global nova_janela
+    nova_janela=tk.Toplevel()
+    nova_janela.config(bg="gray")
+    x=(nova_janela.winfo_screenwidth()//2)-(200//2)
+    y=(nova_janela.winfo_screenheight()//2)-(100//2)
+    nova_janela.geometry(f"{200}x{100}+{x}+{y}")
+    criar_pasta = tk.Button(nova_janela, text="Pasta", command=lambda: (selecao_de_arquivo(1), criar_arquivo.pack_forget(), criar_pasta.pack_forget()))
     criar_pasta=criar_pasta
     criar_pasta.pack(pady=10)
-    criar_arquivo=tk.Button(asd, text="Arquivo", command=lambda:ok(2))
+    criar_arquivo = tk.Button(nova_janela, text="Arquivo", command=lambda: (selecao_de_arquivo(2), criar_arquivo.pack_forget(), criar_pasta.pack_forget()))
     criar_arquivo.pack()
-    asd.mainloop()
+    nova_janela.protocol("WM_DELETE_WINDOW", valor_pasta_0)
+    nova_janela.mainloop()
 
-def criar():
+def criar_um_novo_arquivo(a):
     global pasta
     texto=campo.get()
     if texto!="":
-        for i in range(len(os.listdir())):
-            arquivo[i].destroy()
-        if pasta==1:
-            os.mkdir(texto)
-        else:
-            with open(f"{texto}.py", "w"):
-                pass
-        listar()
+        limpar_arquivos_da_tela()
+        verificar_o_arquivo_selecionado(texto)
     campo.pack_forget()
     confirmar.pack_forget()
-    asd.destroy()
+    nova_janela.destroy()
     pasta=0
 
 #função que lista todos os arquivos presentes na pasta
 def listar():
     arquivo.append("")
-
     i = 0
     linha = 0
     coluna = 0
-
-    for item in os.listdir():
-
-        caixa = tk.Frame(
-            frame_arquivos,
-            width=120,
-            height=140,
-            bg="#2e2d2d"
-        )
-        caixa.grid(row=linha, column=coluna, padx=10, pady=10)
-        caixa.grid_propagate(False)
-
-        arquivo[i] = tk.Label(
-            caixa,
-            text=item,
-            compound="top",
-            wraplength=100,
-            bg="#2e2d2d",
-            fg="white",
-            font=("Segoe UI Variable Small Semibol", 10)
-        )
-
-        if item.endswith(".py"):
-            arquivo[i].config(image=imagem)
-        else:
-            arquivo[i].config(image=diretorio)
-
+    for item in os.listdir(pasta_atual):
+        criar_arquivos_e_adicionar_ao_vetor(linha, coluna, i, item)
+        verificar_arquivo_e_adiciona_imagem(item, i)   
         arquivo[i].pack(expand=True)
-
         coluna += 1
-
         if coluna == 6:
             coluna = 0
             linha += 1
-
-        
         i += 1
+
+
 #cria a janela principal
 janela=tk.Tk()
 janela.geometry("500x300")
@@ -113,30 +151,31 @@ diretorio = tk.PhotoImage(file=caminho("arquivos_do_sistema/pasta.png"))
 
 #vetor que armazena o nome de todos os arquivos da pasta
 arquivo=[""]*len(os.listdir())
-
+pl=tk.Button(janela, text="teste")
+pl.pack(pady=1)
 #botão que abre a criação de um novo arquivo
-botao=tk.Button(janela, command=file, image=mais, bg="#2e2d2d", bd=0)
-botao.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
-
+botao_criacao=tk.Button(janela, command=abrir_janela_ciação_arquivo, image=mais, bg="#2e2d2d", bd=0)
+botao_criacao.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
 #método usado para organizar os arquivos em linhas 
 frame_arquivos = tk.Frame(janela, bg="#2e2d2d")
 frame_arquivos.pack()
-
+pasta_atual="."
 #chama a função que lista todos os arquivos presentes na pasta
 listar()
 
-asd=None
+nova_janela=None
+edit=None
 
 #campo para digitar o nome do arquivo
 campo=None
 confirmar=None
 
-#variável para definir a criação de pastas ou arquivos
+#variável para definir a criação de pastas ou arquivos, que seram transformadas em botões
 criar_arquivo=None
 criar_pasta=None
 
 #variáveis gloais para armazenas os valores d acriação de pasta ou arquivos
 pasta=0
 arquivo_py=0
-
+janela.protocol("WM_DELETE_WINDOW", fechar_todas_janelas)
 janela.mainloop()
